869020022fb6c96dc4ceec3ba2abb50817fd4962
Monish Saravana <monish.sd.2008@Gmail.com>
2023-03-10 20:48:46 -0500

    Added panning

--- main.py
+++ main.py
@@ -12,8 +12,7 @@
 # player = start()
 # def update():
 #     updateFunc(player)
-
-
+import PIL
 from ursina import *
 
 def addPlayerSymbol():
@@ -21,17 +20,23 @@ def addPlayerSymbol():
     b.on_mouse_enter = Func(setattr, b, 'text', 'Hovering')
     b.on_mouse_exit = Func(setattr, b, 'text', 'Not Hovering')
 
+initPos = Vec3(0,0,0)
+
 app = Ursina()
-initPos = Vec3(1,1,1)
-player = Entity(model = "TicTacToeBase.obj", color=color.white, scale=.3, position=initPos)
+Cursor(texture='cursor', scale=.1)
+mouse.visible = False
+window.exit_button.visible = False
+window.fps_counter.enabled = False
+
+player = Entity(parent=camera.ui,  model = "TicTacToeBase.obj", color=color.light_gray, scale=.04, position=initPos)
+# bg = Entity(parent=camera.ui, model='quad', texture=, scale_x=camera.aspect_ratio, z=1)
+
 addPlayerSymbol()
 def update():
-    player.x += held_keys['d']*0.1
-    player.x -= held_keys['a']*0.1
-    player.y += held_keys['w'] * 0.1
-    player.y -= held_keys['s'] * 0.1
-    player.rotation_x += held_keys["r"] * 5
-    player.rotation_y +=  held_keys["r"] * 5
+    # print(mouse.velocity)
+    player.rotation_x += mouse.delta[1]
+    player.rotation_y -= mouse.delta[0]
+    player.rotation_z += mouse.delta[2]
 
 
 
